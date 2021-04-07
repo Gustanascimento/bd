efe7004c55baadd5d589e03156c408152b8dc668
Gustavo Nascimento <56653979+Gustanascimento@users.noreply.github.com>
2021-04-07 04:21:10 -0300

    Add files via upload

---
+++ atualizarEstados.sql
@@ -0,0 +1,19 @@
+-- Atualizar. No caso do UPDATE, nao esquecer do Where!!!!!!
+
+update estados
+set nome = 'Maranhão'
+where sigla = 'MA'
+
+select nome from estados where sigla = 'MA'
+
+update estados
+set nome = 'Paraná', 
+    populacao = 11.32
+where sigla = 'PR'
+
+select 
+    nome, 
+    sigla, 
+    populacao 
+from estados 
+where sigla = 'pr'
